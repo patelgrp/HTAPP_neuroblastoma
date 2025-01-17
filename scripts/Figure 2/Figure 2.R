c1fc23ac7689785f10e08964bc055363f06b261d
Anand Patel lab <anand.patel2@stjude.org>
2025-01-17 12:06:03 -0600

    added sccoda analysis

--- scripts/Figure 2/Figure 2.R
+++ scripts/Figure 2/Figure 2.R
@@ -4,17 +4,7 @@ library(tidyr)
 library(scCustomize)
 library(MetBrewer)
 library(viridis)
-
-#a function for calculating jaccard similarity for gene sets
-jaccard_index <- function(a, b) {
-  unique_a <- unique(a)
-  unique_b <- unique(b)
-  intersection <- length(intersect(unique_a, unique_b))
-  union <- length(union(unique_a, unique_b))
-  if (union == 0)
-    return (NA)
-  return(intersection / union)
-}
+library(SCpubr)
 
 #read in combined dataset of integrated neuroblastoma data
 gcdata <-
@@ -50,7 +40,7 @@ print(
 )
 dev.off()
 
-#pull top 100 feature embeddings for each NMF cluster
+#pull top 50 feature embeddings for each NMF cluster for supplemental table S1
 nmf.embeddings <- matrix(nrow = 50, ncol = 20)
 colnames(nmf.embeddings) <- paste0("iNMF", 1:20)
 for (i in 1:20)
@@ -60,23 +50,13 @@ for (i in 1:20)
                 dim = i,
                 nfeatures = 50)
 }
+write.csv(
+  nmf.embeddings,
+  file = paste0(output.dir, "/NMF embeddings.csv"),
+  row.names = FALSE
+)
 
-# jaccard.nmf <-
-#   outer(
-#     1:ncol(nmf.embeddings),
-#     1:ncol(nmf.embeddings),
-#     Vectorize(function(i, j)
-#       jaccard_index(nmf.embeddings[, i], nmf.embeddings[, j]))
-#   )
-#
-# jaccard.dist.nmf <- 1 - jaccard.nmf
-#
-# hc.nmf <- hclust(as.dist(jaccard.dist.nmf), method = "average")
-# plot(hc.nmf, main = "Hierarchial Clustering Dendogram", xlab = "NMF", sub = "", cex = 0.8)
-#
-# nmf.embeddings <- nmf.embeddings[, hc.nmf$order]
-#pull the top 25 feature loadings for each NMF and merge them (removing duplicates)
-
+#reorder clusters for heatmap
 ordering <- c(
   "2",
   "3",
@@ -95,25 +75,29 @@ ordering <- c(
   "5",
   "4",
   "1",
-  "13",
   "20",
-  "15"
+  "15",
+  "13"
 )
 nmf.embeddings <-
   nmf.embeddings[, paste0("iNMF", ordering)]
-gene.list <- unique(as.vector(nmf.embeddings[25:50,]))
 
+#limit genes for heatmap to top 25 from each NMF (and remove duplicates)
+gene.list <- unique(as.vector(nmf.embeddings[25:50, ]))
+
+#average expression for each NMF cluster
 Idents(gcdata) <- "inmfNorm.cluster"
 avg.neuroblastoma <- AverageExpression(gcdata,
                                        return.seurat = T)
 custom.order <- paste0("g", ordering)
-
 avg.neuroblastoma@active.ident <-
   factor(avg.neuroblastoma@active.ident, levels = custom.order)
+
 color.scheme <-
   colorRampPalette(RColorBrewer::brewer.pal(11, "RdBu"))(256)
 
-pdf(paste0(output.dir, "\GEP heatmap.pdf"))
+#plot heatmap of major genes
+pdf(paste0(output.dir, "/GEP heatmap.pdf"))
 print(
   DoHeatmap(
     avg.neuroblastoma,
@@ -125,10 +109,13 @@ print(
 )
 dev.off()
 
-#pulls cell ids for every sample which had numbat calling
+#pulls cell ids for every sample which had numbat calling and remove RBCs
 gcdata.subset <-
-  subset(gcdata, subset = malignant_calling == "SNV+CNV")
+  subset(gcdata, subset = malignant_calling == "SNV+CNV" &
+           annotated_coarse != 'erythrocyte')
 
+#feature plot of allel probabilities for those cells that had numbat calls.
+pdf(paste0(output.dir, "/malignant state calls.pdf"))
 FeaturePlot_scCustom(
   seurat_object = gcdata.subset,
   features = "allele",
@@ -136,5 +123,40 @@ FeaturePlot_scCustom(
   pt.size = 2,
   raster = T,
   raster.dpi = c(2048, 2048),
-  colors_use = viridis(n=10, option = "D", alpha = 0.7)
+  colors_use = viridis(n = 10, option = "D", alpha = 0.7)
+)
+dev.off()
+
+pdf(paste0(output.dir, "/malignant state violin plot.pdf"))
+VlnPlot_scCustom(
+  seurat_object = gcdata.subset,
+  features = "allele",
+  group.by = "annotated_coarse",
+  split.by = "condition",
+  plot_boxplot = FALSE,
+  pt.size = 0,
+  plot_median = FALSE,
+  colors_use = c("#6FA7B9", "#D1B07C")
+)
+dev.off()
+
+pdf(paste0(output.dir, "/barplots.pdf"))
+do_BarPlot(
+  sample = gcdata.subset,
+  split.by = "condition",
+  group.by = "annotated_coarse",
+  position = "fill",
+  colors.use = c(
+    "endothelium" = met.brewer("Hokusai3", 5)[1],
+    "immune" = met.brewer("Hokusai3", 5)[3],
+    "malignant" = met.brewer("Hokusai3", 5)[4],
+    "stroma" = met.brewer("Hokusai3", 5)[5]
+  )
+)
+dev.off()
+
+#write table for scCODA analyses
+write.csv(
+  table(gcdata.subset$annotated_coarse, gcdata.subset$orig.ident),
+  file = paste0(output.dir, "/cell counts.csv")
 )
